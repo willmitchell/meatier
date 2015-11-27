d35e2c9f77ad13b649ce1d80c150613644e65331
mattkrick <matt.krick@gmail.com>
2015-11-27 13:00:14 -0600

    begin working on prod

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -13,8 +13,6 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
-    new ExtractTextPlugin('style.css', {allChunks: true}),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
@@ -30,25 +28,19 @@ module.exports = {
   ],
   module: {
     loaders: [
+      {test: /\.json$/, loader: 'json-loader'},
+      {test: /\.txt$/, loader: 'raw-loader'},
+      {test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/, loader: 'url-loader?limit=10000'},
+      {test: /\.(eot|ttf|wav|mp3)$/, loader: 'file-loader'},
       {
-        test: /\.json$/,
-        loader: 'json-loader'
-      }, {
-        test: /\.txt$/,
-        loader: 'raw-loader'
-      }, {
-        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-        loader: 'url-loader?limit=10000'
-      }, {
-        test: /\.(eot|ttf|wav|mp3)$/,
-        loader: 'file-loader'
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
       },
-      { test: /\.css$/, loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'},
       //{ test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
-      {test: /\.js$/,
-        loader: 'babel-loader',
+      {
+        test: /\.js$/, loader: 'babel-loader',
         include: path.join(__dirname, ".."),
-        //exclude: /node_modules/,
+        exclude: /node_modules/,
         query: {
           "stage": 0,
           "plugins": ["react-transform"],
---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,72 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    Kanban: ['./src/client/client.js', 'webpack-hot-middleware/client']
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, '..', 'build'),
+    publicPath: '/static/'
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css', {allChunks: true}),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.json', '.jsx']
+  },
+  node: { //used for joi validation on client
+    dns: 'mock',
+    net: 'mock'
+  },
+  postcss: [
+    require('postcss-modules-values')
+  ],
+  module: {
+    loaders: [
+      {test: /\.json$/, loader: 'json-loader'},
+      {test: /\.txt$/, loader: 'raw-loader'},
+      {test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/, loader: 'url-loader?limit=10000'},
+      {test: /\.(eot|ttf|wav|mp3)$/, loader: 'file-loader'},
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
+      },
+      {
+        test: /\.js$/, loader: 'babel-loader',
+        include: path.join(__dirname, ".."),
+        exclude: /node_modules/,
+        query: {
+          "stage": 0,
+          "plugins": ["react-transform"],
+          "extra": {
+            "react-transform": {
+              "transforms": [{
+                "transform": "react-transform-hmr",
+                "imports": ["react"],
+                "locals": ["module"]
+              }, {
+                "transform": "react-transform-catch-errors",
+                "imports": ["react", "redbox-react"]
+              }]
+            }
+          }
+        }
+      }
+    ]
+  }
+};
